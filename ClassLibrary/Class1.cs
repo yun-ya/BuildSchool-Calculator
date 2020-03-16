ea695dd0f8fee6a942985e8dbd90842acb7e4939
施 君翰 <aa4152@gmail.com>
2020-03-16 15:16:14 +0800

    Math

--- ClassLibrary/Class1.cs
+++ ClassLibrary/Class1.cs
@@ -6,42 +6,35 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
-    public class Sum
+    public class Math
     {
         public double Mysum(double a, double b)
         {
             return a + b;
         }
-    }
-    public class Miner 
-    {
-        public double MyMiner(double a,double b)
+        public double MyMiner(double a, double b)
         {
             return a - b;
-        } 
-    }
-    public class multiply
-    {
-        public double Mymultiply(double a,double b)
+        }
+        public double Mymultiply(double a, double b)
         {
             return a * b;
         }
-    }
-    public class Divided
-    {
-        public double Mydivided(double a,double b)
+        public double Mydivided(double a, double b)
         {
             return a / b;
         }
-    }
-    public class PI
-    {
-        public double Mypi (double a)
+        public double Mypi(double a)
         {
             return a * 3.14159265359;
         }
     }
 
-    
-     
+
+
+
+
+
+
+
 }
