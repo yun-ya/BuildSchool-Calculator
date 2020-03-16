cfa0a092ef32ff5eb85521c85a67a9a8e7721da8
施 君翰 <aa4152@gmail.com>
2020-03-16 14:50:43 +0800

    first

--- ClassLibrary/Class1.cs
+++ ClassLibrary/Class1.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
-    public class Class1
+    public class Sum
     {
+        public double Mysum(double a, double b)
+        {
+            return a + b;
+        }
     }
+    public class Miner 
+    {
+        public double MyMiner(double a,double b)
+        {
+            return a - b;
+        } 
+    }
+    public class ccc
+    {
+        public double Myccc(double a,double b)
+        {
+            return a * b;
+        }
+    }
+    public class ddd
+    {
+        public double Myddd(double a,double b)
+        {
+            return a / b;
+        }
+    }
+     
 }
